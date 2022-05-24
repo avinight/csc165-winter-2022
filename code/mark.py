1539a0c6e1a5367c4351ad01bbad4d83f487ba04
Frederick Meneses <75036259+avinight@users.noreply.github.com>
2022-05-23 20:38:58 -0400

    update

--- code/mark.py
+++ code/mark.py
@@ -5,12 +5,11 @@ def get_165() -> float:
     ps1 = float(97.22)
     ps2 = float(98.39)
     ps3 = float(99.22)
-    ps4 = float(99)
+    ps4 = float(98.28)
     tt1 = float(76.72)
+    # tt1 = float(90)
     tt2 = float(90.56)
-    # tt1 = float(70)
-    # tt2 = float(90)
-    final = float(33)
+    final = float(80)
     all_p = [ps1, ps2, ps3, ps4]
     all_p.sort()
     worst_p = all_p[0]
@@ -20,3 +19,18 @@ def get_165() -> float:
 
     return ps0*0.01 + preps*0.03 + best_p*0.08 + worst_p*0.06 + mid1_p*0.07 \
            + mid2_p*0.07 + tt1*0.17 + tt2*0.17 + final*0.34
+
+
+def get_148() -> float:
+    preps = float(88.61)
+    labs = float(88.89)
+    survey = 100
+    a0 = float(96.38)
+    a1 = float(99.11)
+    a2 = float(97.4)
+    midterm = float(95.71)
+    # final = 90
+    final = float(94.74)
+
+    return preps*0.1 + labs*0.09 + survey*0.01 + a0*0.05 + a1*0.13 + a2*0.13 + \
+           midterm*0.15 + final*0.34
